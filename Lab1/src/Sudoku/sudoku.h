fdbf01907468f2e238cc46f6b34fe760c97b3c6b
Deng-bin <2286149377@qq.com>
2020-03-30 12:02:21 +0800

    dengbin change the stdout

--- Lab1/src/Sudoku/sudoku.h
+++ Lab1/src/Sudoku/sudoku.h
@@ -2,24 +2,23 @@
 #define SUDOKU_H
 
 const bool DEBUG_MODE = false;
-enum { ROW=9, COL=9, N = 81, NEIGHBOR = 20 };
-const int NUM = 9;
 
-extern int neighbors[N][NEIGHBOR];
+const int NUM = 9;
+const int kMaxNodes = 1 + 81*4 + 9*9*9*4;
+const int kMaxColumns = 400;
+const int kRow = 100, kCol = 200, kBox = 300, N = 81;
+
+
+
 extern int board[N];
 extern int spaces[N];
 extern int nspaces;
-extern int (*chess)[COL];
 
-void init_neighbors();
 void input(const char in[N]);
 void init_cache();
 
-bool available(int guess, int cell);
-
-bool solve_sudoku_basic(int which_space);
-bool solve_sudoku_min_arity(int which_space);
-bool solve_sudoku_min_arity_cache(int which_space);
-bool solve_sudoku_dancing_links(int unused);
+void trans(const char in[N], int* target);
+bool solve_sudoku_dancing_links(int*);
 bool solved();
+
 #endif
