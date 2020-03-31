af5241feed2559e9a7e9c3943a6ef06bb301378c
jjdjz <2495056615@qq.com>
2020-03-31 09:30:16 -0700

    LAB1 LAST 2020-4-1-0:30

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -17,7 +17,7 @@ int **buf= new int* [lim_puzzle];
 int **buf1= new int* [lim_puzzle];
 bool *is_solved= new bool [lim_puzzle];
 char puzzle[2000000];
-int line[2000000]; 
+int line[2000000];
 
 int64_t now()
 {
@@ -38,34 +38,28 @@ int main(int argc, char* argv[])
 {
     FILE *fp;
     FILE *fp1;
-//    char puzzle[128000];
     int total_solved = 0;
     int total = 0;
-//    buf = new int* [lim_puzzle];
-//    buf1 = new int* [lim_puzzle];
-//    is_solved = new bool [lim_puzzle];
     for(int i = 0; i < lim_puzzle; ++i) buf[i] = new int [N];
     for(int i = 0; i < lim_puzzle; ++i) buf1[i] = new int [N];
     int end_of_file = 0;
     thread_num = thread::hardware_concurrency();
     thread th[thread_num];
-    //char name[10000];
     string name;
     int K=0;
     int T=0;
     char ch;
-//    int line[1024]; 
     line[0]=0;
-    
+
     int kkk=0;
     string name_1[1024];
     while (getline(cin, name)){
     	name_1[kkk]=name;
     	kkk++;
 	}
-    
-    int64_t start = now();
-    
+
+//    int64_t start = now();
+
     int kk=0;
     while (kk<kkk) {
     	name=name_1[kk];
@@ -73,13 +67,11 @@ int main(int argc, char* argv[])
     	line[K]==line_count;
     	K++;
     	char *tmp_name = new char[40];
-   		// name = prefix + name;
     	name = name;
     	strcpy(tmp_name, name.c_str());
     	fp = fopen(tmp_name, "r");
      	if (fp == NULL) {
       		printf("File cannot open!not good!\n");
-     		// exit(0);
     	}
     	end_of_file = 0;
     	while(end_of_file!=1)
@@ -99,8 +91,7 @@ int main(int argc, char* argv[])
 					++total;
         	    }
         	}while(line[K] < lim_puzzle-1);
-		} 
-		//cout<<"file"<<K<<":"<<line[K]<<endl;
+		}
 		for(int i = 0; i < line[K]; ++i)
     	{
         	is_solved[i] = solve_sudoku_dancing_links(buf[i]);
@@ -111,7 +102,7 @@ int main(int argc, char* argv[])
     	{
         	th[i] = thread(thread_work, curr, ((curr+step>=line[K])?(line[K]-curr):step));
     	}
-    	for(int i = 0; i < thread_num; ++i) 
+    	for(int i = 0; i < thread_num; ++i)
 		{
 			th[i].join();
     	}
@@ -121,41 +112,34 @@ int main(int argc, char* argv[])
     	{
         	if(is_solved[i])
         	{
-            	for(int j = 0; j < N; j++) 
+            	for(int j = 0; j < N; j++)
 				{
-				//	 out << buf[i][j];
 				buf1[i+T-line[K]][j]=buf[i][j];
-				//	 cout << buf[i][j];	 
 				}
-            	//  cout<<'\n';
 				++total_solved;
         	}
             else cout<<"No result.\n";
     	}
     kk++;
-	}           
-    
+	}
+
     int sum=0;
     for(int l=0;l<=K;l++)
     {
         sum=sum+line[l];
 	}
-	//cout<<"sum"<<sum<<endl;
-	int num=0; 
+	int num=0;
 	for(int d=0;d<sum;d++)
 	{
 		num++;
-		for(int j = 0; j < N; j++) 
+		for(int j = 0; j < N; j++)
 		{
 		  	cout << buf1[d][j];
 		}
 		cout<<endl;
 	}
-	//  cout<<"num:"<<num<<endl;
-	//	  cout<<"T:"<<T<<endl;
-	//	  cout<<buf1[1003][0]<<" "<<buf1[1003][1]<<" "<<buf1[1003][2]<<" "<<buf1[1003][4]<<endl; 
-    int64_t end = now();
-    double sec = (end-start)/1000000.0;
-    printf("%f sec %f ms each %d\n", sec, 1000.0*sec/total, total_solved);
+//    int64_t end = now();
+//    double sec = (end-start)/1000000.0;
+//   printf("%f sec %f ms each %d\n", sec, 1000.0*sec/total, total_solved);
     return 0;
 }
